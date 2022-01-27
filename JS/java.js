238bdac020ab2d321167253e1de2cb55c8cf2376
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2022-01-27 11:38:11 -0500

    Add files via upload

---
+++ JS/java.js
@@ -0,0 +1,10 @@
+function openNav() {
+	var x=document.getElementById("mobileLinks");
+	if(x.style.display==="block"){
+		x.style.display="none";
+	}
+	else{
+		x.style.display="block";
+	}
+}
+      
